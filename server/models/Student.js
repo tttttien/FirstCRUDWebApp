babe74bb173e3a328f2262bda4b698d87e4d1177
Tien <>
2024-06-03 03:46:10 +0700

    them client

--- server/models/Student.js
+++ server/models/Student.js
@@ -1,21 +1,9 @@
 module.exports = (sequelize, DataTypes) => {
   const Student = sequelize.define('Student', {
-    StudentID: {
-      type: DataTypes.STRING,
-      primaryKey: true,
-      allowNull: false
-    },
+   
     FullName: {
       type: DataTypes.STRING,
       allowNull: false
-    },
-    Gender: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    StudentDOB: {
-      type: DataTypes.DATEONLY,
-      allowNull: true
     },
     StudentAddress: {
       type: DataTypes.STRING,
