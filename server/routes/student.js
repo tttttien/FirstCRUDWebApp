972e7806bd6057a2b81de59b19ec4c1174c82094
Tien <>
2024-06-03 09:26:08 +0700

    them update delete

--- server/routes/student.js
+++ server/routes/student.js
@@ -20,4 +20,41 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const { id } = req.params; // Correctly extract the id parameter
+    const student = await Student.findByPk(id); // Use findByPk method
+
+    if (!student) {
+      return res.status(404).json({ error: "student not found" });
+    }
+
+    await student.destroy();
+    res.status(200).json("success");
+  } catch (error) {
+    console.error("Error deleting student:", error);
+    res.status(500).json({ error: "An error occurred while deleting the student" });
+  }
+});
+
+router.put("/update", async (req, res) => {
+  try {
+    const {id, FullName,StudentPhone,StudentAddress } = req.body; // Correctly extract the quantity parameter
+    const student = await Student.findByPk(id); // Use findByPk method
+    
+    if (!student) {
+      return res.status(404).json({ error: "student not found" });
+    }
+    student.FullName = FullName;
+    student.StudentPhone = StudentPhone;
+    student.StudentAddress= StudentAddress;
+
+    await student.save();
+    res.status(200).json("success");
+  } catch (error) {
+    console.error("Error updating student:", error);
+    res.status(500).json({ error: "An error occurred while updating the student" });
+  }
+});
+
 module.exports = router;
